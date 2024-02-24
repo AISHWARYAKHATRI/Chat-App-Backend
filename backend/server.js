b46bd3b3ec1b8eecba3a3eecba8d73d3ad5996d2
AISHWARYAKHATRI <191200107021@asoit.edu.in>
2024-02-24 19:49:38 +0530

    Login and registration api done for chat app

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const chats = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+dotenv.config();
+
+connectDB();
+const app = express();
+
+app.use(express.json()); // to accept JSON Data
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.use("/api/user", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, console.log(`Server started on Port ${PORT}`.yellow.bold));
